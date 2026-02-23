52819ea4a422a30e4292c4d09c3153402c5831e3
JBernal <j.bernal@upm.es>
2026-02-23 09:23:19 +0100

    fixed test

--- src/main/java/es/upm/api/configurations/ResourceServerConfig.java
+++ src/main/java/es/upm/api/configurations/ResourceServerConfig.java
@@ -1,6 +1,6 @@
 package es.upm.api.configurations;
 
-import es.upm.api.infrastructure.resources.SystemResource;
+import es.upm.api.resorces.SystemResource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/test/java/es/upm/api/functionaltests/SystemResourceFT.java
+++ src/test/java/es/upm/api/functionaltests/SystemResourceFT.java
@@ -1,6 +1,6 @@
 package es.upm.api.functionaltests;
 
-import es.upm.api.infrastructure.resources.SystemResource;
+import es.upm.api.resorces.SystemResource;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
